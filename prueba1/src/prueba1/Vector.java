13e274cbdbe8dd3104cffb3c173f97c029eabbff
monseg73 <monsegcarretero@gmail.com>
2022-01-02 10:27:01 +0100

    Validacion de prueba1 con clase vector

--- prueba1/src/prueba1/Vector.java
+++ prueba1/src/prueba1/Vector.java
@@ -5,16 +5,16 @@ public class Vector {
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
 		int n=5,result=0;
-		int a[]=new int[5];
-		for (int i=0;i<5;i++)
+		int a[]=new int[n];
+		for (int i=0;i<n;i++)
 			a[i]=(i+1)*10;
 		
 		//visualizamos el vector
-		for (int i=0;i<5;i++)
+		for (int i=0;i<n;i++)
 			System.out.printf("Elemento %d=%d\n",i,a[i]);
-		for (int i=0;i<5;i++)
+		for (int i=0;i<n;i++)
 			result=result + a[i];
-		System.out.println("La media es ="+ result/5);
+		System.out.println("La media es ="+ result/n);
 	}
 
 }
